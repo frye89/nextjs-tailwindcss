1d3a23b567b45db7b41c2cf607fde993353bb963
Jeffrey <jeffrey.meus@live.be>
2020-04-26 19:55:46 +0200

    tailwindcss added

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="container mx-auto text-center">
+      <Head>
+        <title>Create Next App & Tailwindcss</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="text-4xl pt-32">
+          Welcome to{" "}
+          <span className="text-blue-700 font-semibold">Next.js</span>, with{" "}
+          <span className="text-green-700 font-semibold">Tailwindcss!</span>
+        </h1>
+      </main>
+    </div>
+  );
+}
