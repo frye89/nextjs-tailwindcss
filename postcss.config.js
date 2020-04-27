2008d260ee5c30a9b4a76e5a6146b0e4ef4b699f
Jeffrey Meus <jeffrey.meus@live.be>
2020-04-27 09:14:13 +0200

    Tailwind production build

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  plugins: ["tailwindcss", "postcss-preset-env"],
+  plugins: [
+    "tailwindcss",
+    "postcss-preset-env",
+    [
+      "@fullhuman/postcss-purgecss",
+      process.env.NODE_ENV === "production"
+        ? {
+            content: ["./pages/**/*.js", "./components/**/*.js"],
+            whitelist: ["html", "body"],
+            defaultExtractor: (content) =>
+              content.match(/[\w-/:]+(?<!:)/g) || [],
+          }
+        : false,
+    ],
+  ],
 };
